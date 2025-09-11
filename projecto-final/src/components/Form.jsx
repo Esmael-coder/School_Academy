44b009720a839c3eff0bedf19c56b9247c992f22
Esmael Monteiro <esmaelmonteiro.unicv@gmail.com>
2025-09-11 18:35:11 +0100

    form

--- projecto-final/src/components/Form.jsx
+++ projecto-final/src/components/Form.jsx
@@ -1,10 +1,44 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { TbMessageCircle } from "react-icons/tb";
 import { courses } from '../const/Constants';
 import { FiSend } from "react-icons/fi";
 
 const courseCopy = [...courses]
 export const Form = () => {
+
+    const [nomeMessage, setNomeMessage] = useState()
+    const validateName = (name) => {
+        if (name.trim() == "") {
+            
+            setNomeMessage("preencha este campo.")
+
+        } else if (!isNaN(name) || name.length < 2) {
+
+            setNomeMessage("Introduza um nome válido.")
+
+        } else {
+            setNomeMessage("")
+            return name
+        }
+    }
+
+    const [emailMessage, setEmailMessage] = useState()
+    const validateEmail = (email) => {
+
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+        if (email.trim() === "") {
+            
+            setEmailMessage("preencha este campo.")
+
+        } else if (!regex.test(email)) {
+
+            setEmailMessage("Introduza um email válido.")
+
+        } else {
+            setEmailMessage("")
+            return email
+        }
+    }
 
     return (
         <div>
@@ -26,11 +60,24 @@ export const Form = () => {
                         <div className='flex flex-col gap-4 md:grid md:grid-cols-2'>
                             <div className='flex flex-col gap-1'>
                                 <label htmlFor="nome">Nome completo</label>
-                                <input className='bg-sombra pl-2 p-1.5 rounded-lg' type="text" name='nome' id='nome' placeholder='Seu nome completo' />
+                                <input
+                                    className='bg-sombra pl-2 p-1.5 rounded-lg'
+                                    onChange={(event) => validateName(event.target.value)}
+                                    type="text" name='nome'
+                                    id='nome'
+                                    placeholder='Seu nome completo' />
+                                <span className='text-red-500 pl-3'>{nomeMessage}</span>
                             </div>
                             <div className='flex flex-col gap-1'>
                                 <label htmlFor="email"> Email</label>
-                                <input className='bg-sombra pl-2 p-1.5 rounded-lg' type='email' name='email' id='email' placeholder='exemplo@gmail.com' />
+                                <input 
+                                className='bg-sombra pl-2 p-1.5 rounded-lg'
+                                onChange={(event) => validateEmail(event.target.value)} 
+                                type='email' 
+                                name='email' 
+                                id='email' 
+                                placeholder='exemplo@gmail.com' />
+                                <span className='text-red-500 pl-3'>{emailMessage}</span>
                             </div>
                         </div>
 
@@ -38,7 +85,7 @@ export const Form = () => {
                         <div className='flex flex-col gap-4 md:grid md:grid-cols-2'>
                             <div className='flex flex-col gap-1'>
                                 <label htmlFor="telefone">Telefone</label>
-                                <input className='bg-sombra pl-2 p-1.5 rounded-lg' type="text" name='telefone' id='telefone' placeholder='+351 9xx xxx xxx'/>
+                                <input className='bg-sombra pl-2 p-1.5 rounded-lg' type="text" name='telefone' id='telefone' placeholder='+351 9xx xxx xxx' />
                             </div>
                             <div className='flex flex-col gap-1'>
                                 <label htmlFor="assunto">Assunto</label>
@@ -69,11 +116,11 @@ export const Form = () => {
                         <textarea className='bg-sombra pl-2 p-1.5 rounded-lg' name="mensagem" id="mensagem" rows={6} cols={30} placeholder='Escreva aqui a sua mensagem, dúvidas ou pedido de informações...'></textarea>
                     </div>
 
-                    <button 
-                    className='bg-orange cursor-pointer hover:bg-hover text-white p-2 flex items-center justify-center gap-3 rounded-lg transition-color duration-300 ease-in-out' 
-                    type="submit">
-                    <FiSend/> 
-                    Enviar mensagem
+                    <button
+                        className='bg-orange cursor-pointer hover:bg-hover text-white p-2 flex items-center justify-center gap-3 rounded-lg transition-color duration-300 ease-in-out'
+                        type="submit">
+                        <FiSend />
+                        Enviar mensagem
                     </button>
                 </form>
 

9265fec6d451d4f0e4a876493e3efd556d3991f2
Esmael Monteiro <esmaelmonteiro.unicv@gmail.com>
2025-09-16 18:02:55 +0100

    changes

--- projecto-final/src/pages/cadastro/Cadastro.jsx
+++ projecto-final/src/pages/cadastro/Cadastro.jsx
@@ -1,3 +1,4 @@
+import { section } from 'motion/react-client'
 import React, { useState } from 'react'
 import { AiOutlineExclamationCircle } from 'react-icons/ai'
 
@@ -51,7 +52,7 @@ export const Cadastro = () => {
 
         const regex = /^(?! )[ -~]{6,12}(?<! )$/
 
-        if(password === ""){
+        if (password === "") {
 
             setpasswordMessage("preencha este campo.")
             return false
@@ -74,7 +75,7 @@ export const Cadastro = () => {
 
     const validatePasswordConfirm = (passwordConfirmation) => {
 
-        if(passwordConfirmation === ""){
+        if (passwordConfirmation === "") {
 
             setpasswordConfirmMessage("preencha este campo.")
             return false
@@ -99,84 +100,86 @@ export const Cadastro = () => {
     }
 
     return (
-        <div className='w-full max-w-7xl mx-auto px-4 md:px-6 lg:px-8'>
+        <section>
+            <div className='w-full max-w-7xl mx-auto px-4 md:px-6 lg:px-8'>
 
-            <div className='max-w-[400px] p-4 shadow-[var(--boxShadow2)] mx-auto mt-40 rounded-md'>
-                <h1 className='text-center text-primary font-bold text-2xl mb-5'>Criar conta</h1>
-                <form className='flex flex-col gap-5' onClick={(event)=>handleSubmit(event)}>
+                <div className='max-w-[400px] p-4 shadow-[var(--boxShadow2)] mx-auto mt-40 rounded-md'>
+                    <h1 className='text-center text-primary font-bold text-2xl mb-5'>Criar conta</h1>
+                    <form className='flex flex-col gap-5' onClick={(event) => handleSubmit(event)}>
 
-                    <div className='flex flex-col gap-1'>
-                        <label htmlFor="nome">Nome</label>
-                        <input className='bg-sombra p-2 rounded-md outline-realce'
-                            type="text"
-                            name='nome'
-                            id='nome'
-                            placeholder='nome de utilizador'
-                            onBlur={(event) => validateName(event.target.value)} />
+                        <div className='flex flex-col gap-1'>
+                            <label htmlFor="nome">Nome</label>
+                            <input className='bg-sombra p-2 rounded-md outline-realce'
+                                type="text"
+                                name='nome'
+                                id='nome'
+                                placeholder='nome de utilizador'
+                                onBlur={(event) => validateName(event.target.value)} />
 
-                        <span className='text-red-500 text-sm pl-3 flex items-center gap-2'>
-                            {nomeMessage !== null && <AiOutlineExclamationCircle />}
-                            {nomeMessage}
-                        </span>
-                    </div>
+                            <span className='text-red-500 text-sm pl-3 flex items-center gap-2'>
+                                {nomeMessage !== null && <AiOutlineExclamationCircle />}
+                                {nomeMessage}
+                            </span>
+                        </div>
 
-                    <div className='flex flex-col gap-1'>
-                        <label htmlFor="email">Email</label>
-                        <input className='bg-sombra p-2 rounded-md outline-realce'
-                            type="email"
-                            name='email'
-                            id='email'
-                            placeholder='seu email'
-                            onBlur={(event) => validateEmail(event.target.value)}
-
-                        />
-
-                        <span className='text-red-500 text-sm pl-3 flex items-center gap-2'>
-                            {emailMessage !== null && <AiOutlineExclamationCircle />}
-                            {emailMessage}
-                        </span>
-                    </div>
-
-                    <div className='flex flex-col gap-1'>
-                        <label htmlFor="password">Palavra-passe</label>
-                        <input
-                            className='bg-sombra p-2 rounded-md outline-realce'
-                            type="password"
-                            name='password'
-                            id='password'
-                            placeholder='palavra-passe'
-                            onBlur={(event) => validatePassword(event.target.value)}
-
-                        />
-                        <span className='text-red-500 text-sm pl-3 flex items-center gap-2'>
-                            {passworMessage !== null && <AiOutlineExclamationCircle />}
-                            {passworMessage}
-                        </span>
-                    </div>
-
-                    <div className='flex flex-col gap-1'>
-                        <label htmlFor="passwordConfirmation">Confirmar palavra-passe</label>
-                        <input
-                            className='bg-sombra p-2 rounded-md outline-realce'
-                            type="password"
-                            name='passwordConfirmation'
-                            id='passwordConfirmation'
-                            placeholder='palavra-passe novamente' 
-                            onBlur={(event)=>validatePasswordConfirm(event.target.value)}
+                        <div className='flex flex-col gap-1'>
+                            <label htmlFor="email">Email</label>
+                            <input className='bg-sombra p-2 rounded-md outline-realce'
+                                type="email"
+                                name='email'
+                                id='email'
+                                placeholder='seu email'
+                                onBlur={(event) => validateEmail(event.target.value)}
 
                             />
 
-                        <span className='text-red-500 text-sm pl-3 flex items-center gap-2'>
-                            {passworConfirmMessage !== null && <AiOutlineExclamationCircle />}
-                            {passworConfirmMessage}
-                        </span>
-                    </div>
+                            <span className='text-red-500 text-sm pl-3 flex items-center gap-2'>
+                                {emailMessage !== null && <AiOutlineExclamationCircle />}
+                                {emailMessage}
+                            </span>
+                        </div>
 
-                    <button
-                        type='submit'
-                        className='bg-primary text-white p-2 rounded-md mt-4'>Criar conta</button>
-                </form>
+                        <div className='flex flex-col gap-1'>
+                            <label htmlFor="password">Palavra-passe</label>
+                            <input
+                                className='bg-sombra p-2 rounded-md outline-realce'
+                                type="password"
+                                name='password'
+                                id='password'
+                                placeholder='palavra-passe'
+                                onBlur={(event) => validatePassword(event.target.value)}
+
+                            />
+                            <span className='text-red-500 text-sm pl-3 flex items-center gap-2'>
+                                {passworMessage !== null && <AiOutlineExclamationCircle />}
+                                {passworMessage}
+                            </span>
+                        </div>
+
+                        <div className='flex flex-col gap-1'>
+                            <label htmlFor="passwordConfirmation">Confirmar palavra-passe</label>
+                            <input
+                                className='bg-sombra p-2 rounded-md outline-realce'
+                                type="password"
+                                name='passwordConfirmation'
+                                id='passwordConfirmation'
+                                placeholder='palavra-passe novamente'
+                                onBlur={(event) => validatePasswordConfirm(event.target.value)}
+
+                            />
+
+                            <span className='text-red-500 text-sm pl-3 flex items-center gap-2'>
+                                {passworConfirmMessage !== null && <AiOutlineExclamationCircle />}
+                                {passworConfirmMessage}
+                            </span>
+                        </div>
+
+                        <button
+                            type='submit'
+                            className='bg-primary text-white p-2 rounded-md mt-4'>Criar conta</button>
+                    </form>
+                </div>
             </div>
-        </div>
+        </section>
     )
 }

2a6bb423a1db506ec42013102797c761e12c61be
Esmael Monteiro <esmaelmonteiro.unicv@gmail.com>
2025-09-22 14:14:39 +0100

    cors

--- server/server.js
+++ server/server.js
@@ -5,11 +5,15 @@ import privateRoutes from './routes/private.js'
 import auth from './middleware/auth.js'
 
 const app = express()
-app.use(cors())
 app.use(express.json())
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 app.use('/', publicRoutes)
 app.use('/', auth, privateRoutes)
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, ()=>console.log(`Server online 🟢 na porta ${PORT}`))
+app.listen(PORT, () => console.log(`Server online 🟢 na porta ${PORT}`))
